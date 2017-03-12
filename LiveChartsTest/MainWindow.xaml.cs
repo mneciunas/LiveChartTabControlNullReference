ebb7e7143373d12aa68ba5b50c74d57992e5216f
mneciunas <mindeliszz@gmail.com>
2017-03-12 15:41:21 +0200

    initial

---
+++ LiveChartsTest/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace LiveChartsTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<TabItem> Tabs { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Tabs = new ObservableCollection<TabItem>
+            {
+                new TabItem(new UserControl(), "Empty"),
+                new TabItem(new ChartsView(), "Charts"),
+                new TabItem(new UserControl(), "Empty"),
+            };
+
+            DataContext = this;
+        }
+
+        public class TabItem
+        {
+            public TabItem(ContentControl content, string header)
+            {
+                Content = content;
+                Header = header;
+            }
+
+            public string Header { get; set; }
+            public ContentControl Content { get; set; }
+        }
+    }
+}
